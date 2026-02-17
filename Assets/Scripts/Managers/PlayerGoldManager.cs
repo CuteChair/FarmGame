b620b4f0d23e63c379c087bf4fab490e1f811261
Jules <jules.lhp@gmail.com>
2026-02-17 18:39:16 -0500

    fiexed shp ui size in 1920 x 1080

--- Assets/Scripts/Managers/PlayerGoldManager.cs
+++ Assets/Scripts/Managers/PlayerGoldManager.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.NetworkInformation;
-using UnityEditor.UIElements;
+
 using UnityEngine;
 
 public class PlayerGoldManager : MonoBehaviour
